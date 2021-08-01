0aa0676e16647015f7b3a5bf258db765f0af7f4a
Ekaterina Savchenko <ekaterina.savchenko.cs@gmail.com>
2021-08-01 13:38:30 +0200

    refactoring : initial commit

--- backend/src/main/java/com/webapp/backend/controllers/HomeController.java
+++ backend/src/main/java/com/webapp/backend/controllers/HomeController.java
@@ -1,13 +1,13 @@
 package com.webapp.backend.controllers;
 
-//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin
+@RequestMapping("/api/messages")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home()
     {
         String answer = "Hello from mySpringBoot";
